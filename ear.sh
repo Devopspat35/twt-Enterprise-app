2fb2290086888a584a694a36b2bc824f2b8f0be1
Devopspat35 <patemf2021@yahoo.com>
2024-12-11 06:56:30 -0500

    enterprise-app -  commit

---
+++ ear.sh
@@ -0,0 +1,6 @@
+git init
+git add .
+git commit -m "enterprise-app -  commit"
+git branch -M master
+git remote add org  https://github.com/Devopspat35/twt-enterprise-app.git
+git push -u org  master
